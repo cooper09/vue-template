9f0196bb7d61d00943e8ec56ab94038b6b08a49c
cooper <cooper@BC-MG.localdomain>
2019-07-26 11:32:26 -0400

    add nav bar

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,55 @@
+<template>
+    <span>
+        <v-navigation-drawer app v-model="drawer" class="light-blue darken-2" dark disable-resize-watcher>
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-tile :key="index">
+                        <v-list-tile-content>
+                            {{item.title}}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider :key="`divider-${index}`"></v-divider>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app color="light-blue darken-4" dark>
+            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+            <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
+            <v-btn color="light-blue darken-3" class="hidden-sm-and-down">JOIN</v-btn>
+        </v-toolbar>
+    </span>
+</template>
+
+<script>
+
+export default {
+  name: 'AppNavigation',
+  components: {
+
+  },
+  data() {
+    return {
+      appTitle: 'B+C Conversions',
+      drawer: false,
+      items: [
+        { title: 'Menu' },
+        { title: 'Sign In' },
+        { title: 'Join' }
+       ]
+      };//end return
+    }//end data
+};//end export 
+</script>
+<style scoped>
+  .v-toolbar__title {
+    margin-right: 1em;
+  }
+
+  .v-toolbar-title img {
+  /*  background: url('src/assets/b+c_logo.png') */
+  }
+</style>

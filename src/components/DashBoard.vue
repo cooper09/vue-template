bd1a7b4759903c59cdf5c78521caeedcf617b599
cooper09 <cooperatkmba@gmail.com>
2019-10-08 09:23:58 -0400

    stow the dashboard

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container fluid>
+        <v-layout>
+            <h1>Dashboard</h1>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard',
+    components: {}
+}; //end export
+</script>
+
+<style scoped></style>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,18 +1,22 @@
 <template>
     <div>
+        DashBoard
         <home-plans></home-plans>
         <meal-recipes></meal-recipes>
+        <dash-board></dash-board>
     </div>
 </template>
 
 <script>
 import HomePlans from '@/components/HomePlans';
 import Campaigns from '@/components/Campaigns';
+import DashBoard from '@/components/DashBoard';
 export default {
     name: 'Menu',
     components: {
         HomePlans,
-        Campaigns
+        Campaigns,
+        DashBoard
     }
 }; //end export
 </script>

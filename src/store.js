3277f12c9f6bf4d89ddbeff05373534eed507e98
cooper09 <cooperatkmba@gmail.com>
2019-10-20 18:59:06 -0400

    iterate data

--- src/store.js
+++ src/store.js
@@ -1,17 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    dataURL:'https://sleepy-everglades-99189.herokuapp.com/multiline_facebook_conversions'
+    dataURL:'https://sleepy-everglades-99189.herokuapp.com/multiline_facebook_conversions',
+    startData: []
   },
   mutations: {
-
+    setData(state, payload) {
+      state.startData = payload;
+      alert('Store.setData data: '+ state.startData );
+    },
   },
   actions: {
     async getData({ state, commit }) {
+      //alert("Store.getData: ");
       try {
           let response = await axios.get(`${state.dataURL}`);
           console.log("store - getData:  ", response.data );

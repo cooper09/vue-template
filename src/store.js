5f8eab65dd8c3ec39343e38adcd8b39b48872d6d
cooper09 <cooperatkmba@gmail.com>
2019-10-20 18:48:19 -0400

    added created goodiing in App

--- src/store.js
+++ src/store.js
@@ -5,12 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    dataURL:'https://sleepy-everglades-99189.herokuapp.com/multiline_facebook_conversions'
   },
   mutations: {
 
   },
   actions: {
-
+    async getData({ state, commit }) {
+      try {
+          let response = await axios.get(`${state.dataURL}`);
+          console.log("store - getData:  ", response.data );
+          commit('setData', response.data);
+      } catch (error) {
+          alert('Data Load error: ' + error);
+      }
+    },//end getData
   }
 })

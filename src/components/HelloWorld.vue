4487aff97ffde14b91aae2be5e563795d1b6c539
cooper09 <cooper@sonyainc.net>
2019-12-12 11:12:13 -0500

    working data table with store

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,28 +2,57 @@
   <v-container   class="animated fadeIn">
    
    <v-btn @click="nextPage()">Next Page</v-btn>
-   {{stateData}}
+   {{mydata}}
+
+  <v-data-table
+    v-model="selected"
+    :headers="headers"
+    :items="mydata"
+    :single-select="singleSelect"
+    item-key="name"
+    show-select
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-switch v-model="singleSelect" label="Single select" class="pa-3"></v-switch>
+    </template>
+  </v-data-table>
+
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-  
-  }),
+       singleSelect: false,
+        selected: [],
+        headers: [
+          {
+            text: 'Name',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Address', value: 'address' },
+          { text: 'Email', value: 'email' },
+        ]
+    }),//end data
   computed:{
-      stateData() {
-        return this.$store.state.data;
-      },
+    mydata () {
+      return this.$store.state.data;
+    }
   },
   methods: {
     nextPage() {
       alert("next page please")
       this.$router.push('/about')
-      let hello = "We shall Overcome";
-      this.$store.dispatch('setData', hello );
+      let hello = "We shall Overcome"
+      //this.$store.dispatch('setData', "more stuff");
+    },
+    selected() {
+      alert("Selected Something")
     }
-  }
+  },//end methods
 };//end export
 </script>
 <style scoped>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    data: "The good stuff..."
+    data: [{ name: "john",
+            address:  "two",
+            email: "three"
+        }]
   },
   mutations: {
     setData(state,payload) {

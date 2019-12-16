daac8736e5ac2977b81a0c135816652624812922
cooper <cooper@BC-MG.localdomain>
2019-12-16 12:03:26 -0500

    running LSTM version

--- src/helpers/modulesML.js
+++ src/helpers/modulesML.js
@@ -35,7 +35,7 @@ var modulesML = {
     const trainingData = [
       [10,9,8,7,6,5,4,3,2,1]
     ]
-    const network = new brain.recurrent.LSTMTimeStep();
+    const network = new brain.recurrent.LSTM();
 
     network.train(trainingData , {
       log: stats => {

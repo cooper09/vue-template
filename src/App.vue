ecaffd902ab8a5e88112354df9429189edd0307c
cooper09 <cooper@sonyainc.net>
2020-01-11 11:10:42 -0500

    here we begin

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <v-toolbar dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Shopping Cart</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-item>
+          <v-btn flat></v-btn>
+        </v-toolbar-item>
+    </v-toolbar>
 
     <v-content>
       <HelloWorld/>

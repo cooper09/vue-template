46d17e3b2e9aa2ef0795bef6044d4b6b97a71a11
cooper09 <cooper@sonyainc.net>
2020-01-12 13:59:07 -0500

    working navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+   
     <Navigation />
     <v-content   class="animated fadeIn">
       <p>App.vue</p>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,33 +1,22 @@
 <template>
     <div>
-        <v-navigation-drawer v-model="drawer" app temporary>
-        <v-list>
-            <v-list>
-                <v-list-item-title>
-                Menu
-                </v-list-item-title>
-                
-            </v-list>
-            <v-divider></v-divider>
-
-            <template v-for="(item, index) in items">
-            <v-list :key="index" :href="item.href">
-                <v-list-item-action>
-                <v-icon light v-html="item.icon"></v-icon>
-                </v-list-item-action>
-                <v-list-item-action>
-                <v-list-item-title v-html="item.title"></v-list-item-title>
-                </v-list-item-action>
-            </v-list>
-            </template>
-        </v-list>
-        </v-navigation-drawer>
-
-        <v-toolbar dark>
-            <v-appbar-nav-icon @click="drawer = !drawer"></v-appbar-nav-icon>
-            <v-toolbar-title><a href="/">ShoppingCart</a></v-toolbar-title>
-            <v-spacer></v-spacer>
-
+       <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-title-title>
+              Menu
+            </v-list-title-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider></v-divider>
+      
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Shopping Cart</v-toolbar-title>
+        <v-spacer></v-spacer>
             <v-toolbar-items>
                 <v-btn >
                 Products
@@ -39,7 +28,7 @@
                     </v-badge>
                 </v-btn>
             </v-toolbar-items>
-        </v-toolbar>
+    </v-toolbar>
     </div>
 </template>
 

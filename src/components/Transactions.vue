8da499183a30c5c73cdb184a8eb84698ddcba3fa
cooper09 <cooper@sonyainc.net>
2021-04-18 11:39:40 -0400

    add datagrid to Transacions page

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -2,20 +2,44 @@
   <v-container   class="animated fadeIn container">
     <span class="right"><v-btn color=primary @click="exitPage()">Exit</v-btn></span>
   <h3>Transactions</h3>
-    <h3>Current Block: </h3>{{getSelected}}
+    <span><b>Current Block:</b>  </span><span>{{getSelected}}</span>
     <!-- Get all transactions for this particular block -->
+      <div class="col-md-8 col-md-offset-2">
+        <div id="people">
+            <v-client-table :data="getTransactions" :columns="columns">
+                <template slot="edit" slot-scope="props">
+                   <div>
+                        <a class="fa fa-edit" :href="edit(props.row.id)">thing</a>
+                    </div>
+                </template>
+            </v-client-table>
+        </div>
+    </div>
     <!-- Need a getter to specific data  -->
-    <p>Current block has no transactions B</p>
-    {{this.getTransactions}}
+
+
   </v-container>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import Vue from 'vue';
+import {ServerTable, ClientTable, Event} from 'vue-tables-2';
+
+    Vue.use(ClientTable, {
+        perPage: 3
+    }, false);
+
 export default {
   data: function() {                           // <== changed this line
      return {
         currentChain: this.newChain,
+        columns: [ 'id','from','to', 'amount','timestamp', 'valid'],
+                tableData: [
+                    {id:1, from:"0x01",to:"0x20", amount: "12", timestamp: "now", valid: true },
+                    {id:2, from:"0x01",to:"0x10", amount: "2o", timestamp: "then", valid: false },
+    
+                ]
       }
     },
   methods: {
@@ -26,18 +50,22 @@ export default {
   created() {
     console.log("Let there be light....");
     const current = this.newChain;
+    let selected = "";
+
     //console.log("the defining edge: ", current ) 
     current.map( block => {
       console.log("Block hash: ", block.hash )
       if (block.hash === this.getSelected ) {
         //we have our block gets its transactions
+        selected = this.getSelected
         console.log("Selected transactions: ", block.transactions )
         const blockTxs = block.transactions;
-        this.$store.dispatch('setTransactions', blockTxs);
+        this.$store.dispatch('setSelectedBlock ', selected );
+        //this.$store.dispatch('setTransactions', blockTxs);
       }
     })
 
-    const selected = this.getSelected
+
     console.log("Selected Block: ", selected )
   },
   computed: mapGetters(['getSelected','newChain', 'getTransactions']),
